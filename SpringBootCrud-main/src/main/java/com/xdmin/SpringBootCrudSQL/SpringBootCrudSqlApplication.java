e0cc7eda1ed9cee1dec84789885cad6a7d1a7490
Tanvy Bhola <tanvybhola@gmail.com>
2022-12-31 11:40:40 +0530

    Add files via upload

---
+++ SpringBootCrud-main/src/main/java/com/xdmin/SpringBootCrudSQL/SpringBootCrudSqlApplication.java
@@ -0,0 +1,13 @@
+package com.xdmin.SpringBootCrudSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudSqlApplication.class, args);
+	}
+
+}
---
+++ SpringBootCrud-main/src/test/java/com/xdmin/SpringBootCrudSQL/SpringBootCrudSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdmin.SpringBootCrudSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
